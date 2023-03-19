1215809643e41657f4dd1c1ba52562929838b049
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-03-19 11:28:38 +0700

    add database

---
+++ bnsp/database/bnsp.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Mar 2023 pada 04.54
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bnsp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `beasiswa`
+--
+
+CREATE TABLE `beasiswa` (
+  `id_beasiswa` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `hp` varchar(13) NOT NULL,
+  `semester` varchar(1) NOT NULL,
+  `pilihan` varchar(255) NOT NULL,
+  `berkas` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `beasiswa`
+--
+
+INSERT INTO `beasiswa` (`id_beasiswa`, `nama`, `email`, `hp`, `semester`, `pilihan`, `berkas`) VALUES
+(23612, 'Retno Ekayanti', 'retnoekayanti15@gmail.com', '089650017574', '7', 'Akademik', 'Tanda_tangan-removebg-preview_1.png'),
+(23613, 'Yuliana setyo', 'yuliana@gmail.com', '089650017574', '1', 'seni', 'Ayumanda.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id_mahasiswa` int(11) NOT NULL,
+  `nama_mahasiswa` varchar(255) NOT NULL,
+  `ipk` decimal(3,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id_mahasiswa`, `nama_mahasiswa`, `ipk`) VALUES
+(19700650, 'Retno Ekayanti', '3.89'),
+(19700651, 'Haikal', '3.43'),
+(19700652, 'Megawati', '3.20'),
+(19700653, 'Yuliana Setyo', '3.80');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `beasiswa`
+--
+ALTER TABLE `beasiswa`
+  ADD PRIMARY KEY (`id_beasiswa`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id_mahasiswa`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `beasiswa`
+--
+ALTER TABLE `beasiswa`
+  MODIFY `id_beasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23614;
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19700654;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
